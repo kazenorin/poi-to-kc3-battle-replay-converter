60e73f0e5f80f9619495d1e2d9365de7a8409cec
kazenorin <18181393+kazenorin@users.noreply.github.com>
2017-08-25 15:05:19 +0800

    Separate dev-build from production dist

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(['dev-build']),
         new HtmlWebpackPlugin({
             title: 'POI Battle Detail to KC3Replay Converter (DEV)',
             template: 'src/index.ejs'
@@ -16,7 +16,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dev-build')
     },
     module: {
         rules: [
